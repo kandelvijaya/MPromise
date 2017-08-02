83c5a988aa47dfdbe57321f3970903720b1c64fb
vkandel <vijaya.prakash.kandel@zalando.de>
2017-08-03 00:40:17 +0200

    removed old files

---
+++ PromiseMonad.playground/Contents.swift
@@ -0,0 +1,89 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+
+let url = URL(string: "https://www.kandelvijaya.com")!
+let url2 = URL(string: "https://www.objc.io")!
+
+enum DataConversionError: Error {
+    case dataCannotBeConvertedToString
+}
+
+func dataToString(_ data: Data) -> Result<String> {
+    let string = String(data: data, encoding: .utf8)
+    guard let str = string else {
+        return .failure(DataConversionError.dataCannotBeConvertedToString)
+    }
+    return .success(str)
+}
+
+func takeFirstLine(_ string: String) -> Result<String> {
+    if let index = string.index(of: "\n") {
+        return .success(string.substring(to: index))
+    }
+    return .failure(DataConversionError.dataCannotBeConvertedToString)
+}
+
+
+func takeFirst20Chars(_ string: String) -> Result<String> {
+    let prefixed = String(string.prefix(20))
+    return .success(prefixed)
+}
+
+
+
+
+func testThennedPromisePromise() -> Promise<()> {
+    let x = Network(url).get().then { (data) -> Result<String> in
+            print("Step 1")
+            let ret = data.bind(dataToString)
+            return ret
+        }.then { (str) -> () in
+            print("printing step 2")
+            print(str.bind(takeFirstLine))
+        }.bind { (_) -> Promise<Result<Data>> in
+            print("Step 3: Another task")
+            return Network(url2).get()
+        }.then { (result) -> () in
+            print("Step 4 printing")
+            print(result.bind(dataToString).bind(takeFirst20Chars))
+        }
+    return x
+}
+
+func testThennedPromisePromiseWithoutTypeInfomation() -> Promise<()> {
+    /// If any but the last then or bind block has a print statement, the closure return type
+    /// cannot be inferred. Strange but true.
+    let x = Network(url).get().then {
+            return $0.bind(dataToString)
+        }.then {
+            return ()
+        }.bind {
+            return Network(url2).get()
+        }.then {
+            print($0.bind(dataToString).bind(takeFirst20Chars))
+        }
+    return x
+}
+
+
+func testNestingOfPromises() {
+    testThennedPromisePromise().then {
+        print("inner promise")
+        testThennedPromisePromiseWithoutTypeInfomation().execute()
+    }.execute()
+}
+
+// testThennedPromisePromise().execute()
+// testThennedPromisePromiseWithoutTypeInfomation().execute()
+// testNestingOfPromises()
+
+
+
+
+
+
+
+PlaygroundPage.current.needsIndefiniteExecution = true
---
+++ PromiseMonad.playground/Sources/Promise.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+/// A Functor ==> Monadic type that encapsulates completion block
+/// and their associated async task thereby providing a nice
+/// synchronous and flow oriented programming model.
+public final class Promise<T> {
+
+    public typealias Completion = (T) -> Void
+
+    /// This is the async task that we are abstracting over
+    var aTask: ((Completion?) -> Void)? = nil
+
+    /// When creating a promise, you should care to call the first argument closure
+    /// that is provided with the eventual result value.
+    public init(_ task: @escaping ((Completion?) -> Void)) {
+        self.aTask = task
+    }
+
+    /// `then` is equivalent to `fmap`/`map`. It makes Promises Functorial.
+    /// This deals with synchronous side of the world.
+    /// If you want to create a Prmose inside of then, you are better off
+    /// using `bind`
+    @discardableResult public func then<U>(_ transform: @escaping (T) -> U) -> Promise<U> {
+        return Promise<U>{ upcomingCompletion in
+            self.aTask?() { tk in
+                let transformed = transform(tk)
+                upcomingCompletion?(transformed)
+            }
+        }
+    }
+
+    /// `bind` is equivalent to `>>=`. It makes Promises Monadic
+    /// If all you want is to do synchronous tasks with the result
+    /// its better to use the functorial `then`. However, bind is
+    /// implemented in terms of `then` and `join`.
+    public func bind<U>(_ transform: @escaping (T) -> Promise<U>) -> Promise<U> {
+        let transformed = then(transform)
+        return Promise.join(transformed)
+    }
+
+    static public func join<A>(_ input: Promise<Promise<A>>) -> Promise<A> {
+        return Promise<A>{ aCompletion in
+            input.then { innerPromise in
+                innerPromise.then { innerValue in
+                    aCompletion?(innerValue)
+                }.execute()
+            }.execute()
+        }
+    }
+
+    /// Call this method on any Promise type to execute and fulfill the promise.
+    /// This is important because the design of Promises is to not evaluate immediately
+    /// but create a expression that can be executed/ passed or stored. The expression
+    /// can be internally be optimized or lazily evaluated.
+    public func execute() {
+        aTask?(nil)
+    }
+
+}
